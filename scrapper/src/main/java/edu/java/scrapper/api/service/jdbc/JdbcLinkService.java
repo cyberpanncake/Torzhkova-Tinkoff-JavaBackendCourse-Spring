a939667312f6d1ca00fc790cc1a5fe184bd27e34
cyberpanncake <cloudy9402@gmail.com>
2024-04-05 00:21:20 +0300

    fix: add transactional annotations to jdbc services

--- scrapper/src/main/java/edu/java/scrapper/api/service/jdbc/JdbcLinkService.java
+++ scrapper/src/main/java/edu/java/scrapper/api/service/jdbc/JdbcLinkService.java
@@ -20,7 +20,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class JdbcLinkService extends ScrapperService implements LinkService {
 
