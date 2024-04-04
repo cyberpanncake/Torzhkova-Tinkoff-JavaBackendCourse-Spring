f6a83081617b17b655c4fe0401defe641a77a880
cyberpanncake <cloudy9402@gmail.com>
2024-04-05 00:09:49 +0300

    fix: remove config injection in commands, add bean injection instead

--- bot/src/main/java/edu/java/bot/telegram/command/components/TrackCommand.java
+++ bot/src/main/java/edu/java/bot/telegram/command/components/TrackCommand.java
@@ -1,11 +1,11 @@
 package edu.java.bot.telegram.command.components;
 
 import edu.java.bot.client.service.ScrapperService;
-import edu.java.bot.configuration.CommandConfig;
 import edu.java.bot.telegram.command.AbstractServiceCommand;
 import edu.java.bot.telegram.command.CommandUtils;
 import edu.java.bot.telegram.exception.UnregisteredUserException;
 import edu.java.bot.telegram.exception.parameter.ParameterException;
+import edu.java.dto.utils.LinkParser;
 import edu.java.dto.utils.exception.LinkException;
 import edu.java.dto.utils.exception.LinkRegistrationException;
 import org.springframework.core.annotation.Order;
@@ -14,9 +14,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(3)
 public class TrackCommand extends AbstractServiceCommand {
+    private final LinkParser parser;
 
-    public TrackCommand(ScrapperService service, CommandConfig config) {
-        super(service, config);
+    public TrackCommand(ScrapperService service, LinkParser parser) {
+        super(service);
+        this.parser = parser;
     }
 
     @Override
@@ -34,7 +36,7 @@ public class TrackCommand extends AbstractServiceCommand {
         throws ParameterException, UnregisteredUserException, LinkException {
         CommandUtils.checkParamsNumber(params, 1);
         String link = params[0];
-        config.linkParser().parse(link);
+        parser.parse(link);
         CommandUtils.checkUserRegistration(userId, service);
         if (service.isLinkRegistered(userId, link)) {
             throw new LinkRegistrationException("Ссылка уже зарегистрирована");
