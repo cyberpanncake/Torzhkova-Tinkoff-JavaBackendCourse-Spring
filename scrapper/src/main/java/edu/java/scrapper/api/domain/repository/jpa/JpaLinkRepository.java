a9704b033cba14acb9f15ba571cdfe23970c3ba4
cyberpanncake <cloudy9402@gmail.com>
2024-04-07 12:25:20 +0300

    feat: add jpa link updater service realization

--- scrapper/src/main/java/edu/java/scrapper/api/domain/repository/jpa/JpaLinkRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/api/domain/repository/jpa/JpaLinkRepository.java
@@ -2,6 +2,8 @@ package edu.java.scrapper.api.domain.repository.jpa;
 
 import edu.java.scrapper.api.domain.dto.jpa.Link;
 import java.net.URI;
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface JpaLinkRepository extends JpaRepository<Link, Long> {
     Optional<Link> findByUrl(URI url);
+
+    //    @Query(value = "select * from link where link.last_check < ?", nativeQuery = true)
+    List<Link> findByLastCheckLessThanEqual(OffsetDateTime time);
 }
